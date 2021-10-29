56ca22309075f0b793438fb4b5439da353691748
francienevaz <fran.vfraga@gmail.com>
2021-10-29 18:57:49 -0300

    update

--- palindromo.js
+++ palindromo.js
@@ -23,3 +23,22 @@ function verificarPalindromo2 (string) {
 }
 
 console.log(verificarPalindromo2('abba'))
+
+
+function substituiPares (array) {
+    if (!array) return -1;
+    if (!array.length) return -1;
+    for (let i = 0; i < array.length ; i++){
+        if(array[i] === 0) {
+            console.log('Você já é zero')
+        } else if ( array[i] % 2 === 0) {
+            console.log (`Substituindo ${array[i]} por 0...`);
+            array[i] = 0
+        }
+    }
+    return array;
+}
+
+let arr = [1,2,4,5,7,8,9,41,74,29,54,36,74,91,100]
+console.log (substituiPares(arr))
+
