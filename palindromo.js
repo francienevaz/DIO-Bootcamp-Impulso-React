242c2dba8635663f9f908246944c999938dfa287
francienevaz <fran.vfraga@gmail.com>
2021-10-29 11:42:56 -0300

    verificando palindromo

---
+++ palindromo.js
@@ -0,0 +1,25 @@
+//solução 1
+
+function verificarPalindromo (string) {
+if(!string) return "string inexistente";
+
+ return string.split("").reverse().join("") === string;
+
+}
+
+console.log(verificarPalindromo(""))
+
+//solução 2
+
+function verificarPalindromo2 (string) {
+    if(!string) return "inexistente";
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+console.log(verificarPalindromo2('abba'))
